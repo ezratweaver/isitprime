78a8fb7b278e4ece614a84895b25186fb46ded42
ezratweaver <101545981+ezratweaver@users.noreply.github.com>
2023-04-21 13:02:40 -0500

    Working Dark | Light Mode

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+"""Gui File"""
 from tkinter import Canvas, Entry, Button
 from window import window
 from assets import Assets
@@ -7,6 +8,13 @@ class Gui():
     """Light Mode Class"""
 
     def __init__(self):
+        self.LIGHT_BG = "#FFFFFF"
+        self.DARK_BG = "#202020"
+        self.LIGHT_CALCULATE_COLOR = "#0067C0"
+        self.DARK_CALCULATE_COLOR = "#4043C8"
+
+        self.theme_dark = False
+
         self.main_canvas = Canvas(
             window,
             bg="#FFFFFF",
@@ -16,7 +24,7 @@ class Gui():
             highlightthickness=0,
             relief="ridge")
         self.main_canvas.pack()
-        
+
         self.calculate_bg = self.main_canvas.create_image(
             173.0, 180.0, image=Assets.image_light_calculate_passive)
         self.entry_bg = self.main_canvas.create_image(
@@ -56,15 +64,60 @@ class Gui():
             image=Assets.image_light_theme_switch,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: None,
+            command=self.activate_darkmode,
             relief="flat")
         self.theme_switch.place(x=8.0, y=192.0, width=24.0, height=24.0)
 
     def activate_lightmode(self):
         """Activates Light Mode"""
+        self.theme_dark = False
+        self.main_canvas.configure(
+            bg=self.LIGHT_BG)
+        self.main_canvas.itemconfigure(
+            self.calculate_bg,
+            image=Assets.image_light_calculate_passive)
+        self.main_canvas.itemconfigure(
+            self.entry_bg,
+            image=Assets.image_light_entry_bg)
+        self.main_canvas.itemconfigure(
+            self.title,
+            image=Assets.image_light_title)
+        self.theme_switch.configure(
+            image=Assets.image_light_theme_switch,
+            bg=self.LIGHT_BG,
+            activebackground=self.LIGHT_BG,
+            command=self.activate_darkmode)
+        self.calculate_button.configure(
+            bg=self.LIGHT_CALCULATE_COLOR,
+            fg=self.LIGHT_BG,
+            activebackground=self.LIGHT_CALCULATE_COLOR)
+        self.entrybox.configure(
+            bg="#CCCCCC")
 
     def activate_darkmode(self):
         """Activates Dark Mode"""
+        self.theme_dark = True
+        self.main_canvas.configure(
+            bg=self.DARK_BG)
+        self.main_canvas.itemconfigure(
+            self.calculate_bg,
+            image=Assets.image_dark_calculate_passive)
+        self.main_canvas.itemconfigure(
+            self.entry_bg,
+            image=Assets.image_dark_entry_bg)
+        self.main_canvas.itemconfigure(
+            self.title,
+            image=Assets.image_dark_title)
+        self.theme_switch.configure(
+            image=Assets.image_dark_theme_switch,
+            bg=self.DARK_BG,
+            activebackground=self.DARK_BG,
+            command=self.activate_lightmode)
+        self.calculate_button.configure(
+            bg=self.DARK_CALCULATE_COLOR,
+            fg="#040404",
+            activebackground=self.DARK_CALCULATE_COLOR)
+        self.entrybox.configure(bg="#3B3B3B")
 
     def show_if_prime(self, boolean):
         """Change The Calculate Button To Reflect If Number is Prime"""
@@ -81,26 +134,33 @@ class Gui():
             color = "#BC3838"
         self.calculate_button.configure(
             bg=color, text=responsetext, activebackground=color)
-        global_resettimer = self.main_canvas.after(1000, self.reset_calculate_button)
+        global_resettimer = self.main_canvas.after(
+            1000, self.reset_calculate_button)
 
     def reset_calculate_button(self):
         """Reset Calculate Button Back To Normal"""
+        if self.theme_dark:
+            active_calculate_color = self.DARK_CALCULATE_COLOR
+            active_calculate_image = Assets.image_dark_calculate_passive
+        else:
+            active_calculate_color = self.LIGHT_CALCULATE_COLOR
+            active_calculate_image = Assets.image_light_calculate_passive
         self.calculate_button.configure(
-            bg="#0067C0", text="Calculate", activebackground="#0067C0")
+            bg=active_calculate_color, text="Calculate", activebackground=active_calculate_color)
         self.main_canvas.itemconfigure(
-            self.calculate_bg, image=Assets.image_light_calculate_passive)
-        
+            self.calculate_bg, image=active_calculate_image)
+
     def input_taken(self, event):
         """Ran when user input is accepted"""
         prime_number = self.entrybox.get()
         self.entrybox.delete(0, "end")
         try:
             prime_number = int(prime_number)
-        except:
+        except ValueError:
             return
         try:
             self.main_canvas.after_cancel(global_resettimer)
-        except:
+        except NameError:
             pass
         self.show_if_prime(self.is_it_prime(prime_number))
 
@@ -115,16 +175,11 @@ class Gui():
             return False
 
     def is_it_prime(self, number):
-        """Checking if number is prime"""
+        """Check if number is prime"""
         if number > 1:
             for i in range(2, number):
                 if (number % i) == 0:
                     return False
-            return True
+                return True
         else:
             return False
-
-if __name__ == "__main__":
-    gui = Gui()
-    window.resizable(False, False)
-    window.mainloop()
--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from gui import Gui, window
+from winreg import OpenKey, QueryValueEx, CloseKey, HKEY_CURRENT_USER
+
+def is_dark_mode_enabled():
+    """Checks registry if user is in dark mode"""
+    registry_path = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize'
+    registry_key = 'AppsUseLightTheme'
+    try:
+        reg = OpenKey(HKEY_CURRENT_USER, registry_path)
+        value, registry_type = QueryValueEx(reg, registry_key)
+        CloseKey(reg)
+        return value == 0
+    except WindowsError:
+        return False
 
 
 if __name__ == "__main__":
     gui = Gui()
-    gui.show_lightmode_canvas()
+    if is_dark_mode_enabled():
+        gui.activate_darkmode()
+    window.title("Prime Number Check")
+    window.iconbitmap("isitprime.ico")
     window.resizable(False, False)
     window.mainloop()
